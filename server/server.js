a53d71770d5071e70ce9a4290d4d42891b58c830
Austin Howard <cnfishead@hotmail.com>
2022-04-20 23:48:35 -0400

    trying to fix server

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use("/api/dynamic", dynamicRoutes);
 app.use(express.static(path.join(__dirname, "../public")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
